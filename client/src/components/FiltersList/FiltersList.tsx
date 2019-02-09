d6ecdb52a794bce60cf4c21898074d797b868c85
ivan3123708 <ivan3123708@gmail.com>
2019-02-09 21:06:13 +0100

    Refactor FiltersList

--- client/src/components/FiltersList/FiltersList.tsx
+++ client/src/components/FiltersList/FiltersList.tsx
@@ -1,154 +1,214 @@
 import * as React from 'react';
-import { List, ListItem } from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
-import Checkbox from 'material-ui/Checkbox';
+import List from '@material-ui/core/List';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import { IFilters } from '@typings/state/index';
+import { filterTypes, filterValues } from '@typings/filters';
 import '@styles/FiltersList.css';
 
 interface Props {
   filters: IFilters;
-  setFilter: (name: string, value: string) => void;
+  setFilter: (name: filterTypes, value: filterValues) => void;
 }
 
-class FiltersList extends React.Component<Props> {
-  handleCheck = (e: React.MouseEvent<HTMLInputElement>) => {
-    const { setFilter } = this.props;
+interface State {
+  price: boolean;
+  brand: boolean;
+  color: boolean;
+  os: boolean;
+  internalMemory: boolean;
+  ram: boolean;
+  displaySize: boolean;
+  displayResolution: boolean;
+  camera: boolean;
+  cpu: boolean;
+}
 
-    setFilter(e.currentTarget.name, e.currentTarget.value);
+class FiltersList extends React.Component<Props, State> {
+  state = {
+    price: false,
+    brand: false,
+    color: false,
+    os: false,
+    internalMemory: false,
+    ram: false,
+    displaySize: false,
+    displayResolution: false,
+    camera: false,
+    cpu: false
   };
 
-  render() {
+  filterData = {
+    priceRange: {
+      type: 'priceRange',
+      title: 'Price',
+      filters: [
+        { label: '< $250', value: '<250' },
+        { label: '$250 - $500', value: '250-500' },
+        { label: '$500 - $750', value: '500-750' },
+        { label: '$750', value: '750>' }
+      ]
+    },
+    brand: {
+      type: 'brand',
+      title: 'Brand',
+      filters: [
+        { label: 'Samsung', value: 'samsung' },
+        { label: 'Apple', value: 'apple' },
+        { label: 'Huawei', value: 'huawei' },
+        { label: 'LG', value: 'lg' },
+        { label: 'HTC', value: 'htc' }
+      ]
+    },
+    color: {
+      type: 'color',
+      title: 'Color',
+      filters: [
+        { label: 'Black', value: 'black' },
+        { label: 'White', value: 'white' },
+        { label: 'Grey', value: 'grey' }
+      ]
+    },
+    os: {
+      type: 'os',
+      title: 'OS',
+      filters: [
+        { label: 'Android', value: 'android' },
+        { label: 'iOS', value: 'ios' }
+      ]
+    },
+    internalMemory: {
+      type: 'internalMemory',
+      title: 'Internal Memory',
+      filters: [
+        { label: '16GB', value: '16' },
+        { label: '64GB', value: '64' },
+        { label: '128GB', value: '128' },
+        { label: '256GB', value: '256' }
+      ]
+    },
+    ram: {
+      type: 'ram',
+      title: 'RAM',
+      filters: [
+        { label: '1GB', value: '1' },
+        { label: '3GB', value: '3' },
+        { label: '4GB', value: '4' },
+        { label: '6GB', value: '6' }
+      ]
+    },
+    displaySize: {
+      type: 'displaySize',
+      title: 'Display Size',
+      filters: [
+        { label: '4.5"', value: '4.5' },
+        { label: '5.1"', value: '5.1' },
+        { label: '5.5"', value: '5.5' },
+        { label: '5.8"', value: '5.8' },
+        { label: '6.0"', value: '6.0' },
+        { label: '6.3"', value: '6.3' }
+      ]
+    },
+    displayResolution: {
+      type: 'displayResolution',
+      title: 'Display Resolution',
+      filters: [
+        { label: '540x960', value: '540x960' },
+        { label: '1080x1920', value: '1080x1920' },
+        { label: '1125x2436', value: '1125x2436' },
+        { label: '1440x2560', value: '1440x2560' },
+        { label: '1440x2880', value: '1440x2880' },
+        { label: '1440x2960', value: '1440x2960' }
+      ]
+    },
+    camera: {
+      type: 'camera',
+      title: 'Camera',
+      filters: [
+        { label: '8Mpix', value: '8' },
+        { label: '12Mpix', value: '12' },
+        { label: '13Mpix', value: '13' },
+        { label: '16Mpix', value: '16' }
+      ]
+    },
+    cpu: {
+      type: 'cpu',
+      title: 'CPU',
+      filters: [
+        { label: 'Quad Core', value: 'quad_core' },
+        { label: 'Hexa Core', value: 'hexa_core' },
+        { label: 'Octa Core', value: 'octa_core' }
+      ]
+    }
+  };
+
+  handleCheck = (filterType: filterTypes, filterValue: filterValues) => {
+    const { setFilter } = this.props;
+
+    setFilter(filterType, filterValue);
+  };
+
+  handleCollapse = (target: filterTypes) => {
+    this.setState((prevState: State) => ({
+      ...prevState,
+      [target]: !prevState[target]
+    }));
+  };
+
+  renderListItems = (data: any) => {
+    const { type, title, filters } = data;
     const { filters: { checked } } = this.props;
 
     return (
+      <div>
+        <ListItem button onClick={() => this.handleCollapse(type)}>
+          <ListItemText primary={title} />
+          {this.state[type] ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={this.state[type]} timeout="auto">
+          <List>
+            {filters.map(({ label, value }: any) => (
+              <ListItem>
+                <FormControlLabel
+                  className="label"
+                  control={<Checkbox checked={checked.includes(value)} onChange={() => this.handleCheck(type, value)} />}
+                  label={label}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Collapse>
+      </div>
+    );
+  }
+
+  render() {
+    return (
       <div className="filtersList">
-        <List>
-          <Subheader>Search by:</Subheader>
-          <ListItem
-            className="listItem"
-            primaryText="Price"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="< $250" name="priceRange" value="<250" checked={checked.includes('<250')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="$250 - $500" name="priceRange" value="250-500" checked={checked.includes('250-500')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="$500 - $750" name="priceRange" value="500-750" checked={checked.includes('500-750')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="$750 >" name="priceRange" value="750>" checked={checked.includes('750>')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="Brand"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="Samsung" name="brand" value="samsung" checked={checked.includes('samsung')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="Apple" name="brand" value="apple" checked={checked.includes('apple')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="Huawei" name="brand" value="huawei" checked={checked.includes('huawei')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="LG" name="brand" value="lg" checked={checked.includes('lg')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="HTC" name="brand" value="htc" checked={checked.includes('htc')} onCheck={this.handleCheck} />,
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="Color"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="Black" name="color" value="black" checked={checked.includes('black')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="White" name="color" value="white" checked={checked.includes('white')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="Grey" name="color" value="grey" checked={checked.includes('grey')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="OS"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="Android" name="os" value="android" checked={checked.includes('android')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="iOS" name="os" value="ios" checked={checked.includes('ios')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="Internal memory"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="16GB" name="internalMemory" value="16" checked={checked.includes('16')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="64GB" name="internalMemory" value="64" checked={checked.includes('64')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="128GB" name="internalMemory" value="128" checked={checked.includes('128')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="256GB" name="internalMemory" value="256" checked={checked.includes('256')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="RAM"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="1GB" name="ram" value="1" checked={checked.includes('1')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="3GB" name="ram" value="3" checked={checked.includes('3')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="4GB" name="ram" value="4" checked={checked.includes('4')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="6GB" name="ram" value="6" checked={checked.includes('6')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="Display size"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="4.5''" name="displaySize" value="4.5" checked={checked.includes('4.5')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="5.1''" name="displaySize" value="5.1" checked={checked.includes('5.1')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="5.5''" name="displaySize" value="5.5" checked={checked.includes('5.5')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="5.8''" name="displaySize" value="5.8" checked={checked.includes('5.8')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="6.0''" name="displaySize" value="6.0" checked={checked.includes('6.0')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="6.3''" name="displaySize" value="6.3" checked={checked.includes('6.3')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="Display resolution"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="540x960" name="displayResolution" value="540x960" checked={checked.includes('540x960')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="1080x1920" name="displayResolution" value="1080x1920" checked={checked.includes('1080x1920')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="1125x2436" name="displayResolution" value="1125x2436" checked={checked.includes('1125x2436')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="1440x2560" name="displayResolution" value="1440x2560" checked={checked.includes('1440x2560')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="1440x2880" name="displayResolution" value="1440x2880" checked={checked.includes('1440x2880')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="1440x2960" name="displayResolution" value="1440x2960" checked={checked.includes('1440x2960')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="Camera"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="8Mpix" name="camera" value="8" checked={checked.includes('8')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="12Mpix" name="camera" value="12" checked={checked.includes('12')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="13Mpix" name="camera" value="13" checked={checked.includes('13')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="16Mpix" name="camera" value="16" checked={checked.includes('16')} onCheck={this.handleCheck} />
-            ]}
-          />
-          <ListItem
-            className="listItem"
-            primaryText="CPU"
-            initiallyOpen={false}
-            primaryTogglesNestedList={true}
-            nestedItems={[
-              <Checkbox className="checkbox" label="Quad Core" name="cpu" value="quad_core" checked={checked.includes('quad_core')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="Hexa Core" name="cpu" value="hexa_core" checked={checked.includes('hexa_core')} onCheck={this.handleCheck} />,
-              <Checkbox className="checkbox" label="Octa Core" name="cpu" value="octa_core" checked={checked.includes('octa_core')} onCheck={this.handleCheck} />
-            ]}
-          />
+        <List
+          component="nav"
+          subheader={<ListSubheader component="div" disableSticky={true}>Filter by:</ListSubheader>}
+        >
+          {this.renderListItems(this.filterData.priceRange)}
+          {this.renderListItems(this.filterData.brand)}
+          {this.renderListItems(this.filterData.color)}
+          {this.renderListItems(this.filterData.os)}
+          {this.renderListItems(this.filterData.internalMemory)}
+          {this.renderListItems(this.filterData.ram)}
+          {this.renderListItems(this.filterData.displaySize)}
+          {this.renderListItems(this.filterData.displayResolution)}
+          {this.renderListItems(this.filterData.camera)}
+          {this.renderListItems(this.filterData.cpu)}
         </List>
       </div>
-    )
+    );
   }
-};
+}
 
 export default FiltersList;
